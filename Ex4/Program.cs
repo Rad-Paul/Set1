0f0ed0475b0b41b812a011893b02a4eff78e31c8
Rad-Paul <115529048+Rad-Paul@users.noreply.github.com>
2023-01-30 05:34:44 +0200

    Add files via upload

---
+++ Ex4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Detreminati daca un an y este an bisect.
+            int an;
+            Console.WriteLine("Introduceti anul: ");
+            an = int.Parse(Console.ReadLine());
+            
+
+            if ((an % 400) == 0)
+            {
+                Console.WriteLine(an + " este un an bisect.");
+            }
+
+            else if((an % 100) == 0)
+            {
+                Console.WriteLine(an + " nu este un an bisect.");
+
+            }
+
+            else if(an % 4 == 0)
+            {
+                Console.WriteLine(an + " este un an bisect.");
+            }
+
+            else
+            {
+                Console.WriteLine(an + " nu este un an bisect.");
+            }
+          
+
+        }
+    }
+}
