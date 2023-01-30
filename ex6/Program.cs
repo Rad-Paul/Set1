eba4a69fac1fa17f4e36b14cb05c3e4dcd7edbe5
Rad-Paul <115529048+Rad-Paul@users.noreply.github.com>
2023-01-30 05:36:59 +0200

    Add files via upload

---
+++ ex6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cele trei numere trebuie sa fie pozitive, oricare doua dintre ele sa fie mai mare decat a treia latura
+            int lat1, lat2, lat3;
+            Console.WriteLine("Introduceti lungimea laturilor: ");
+            lat1 = int.Parse(Console.ReadLine());
+            lat2 = int.Parse(Console.ReadLine());
+            lat3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Triunghi(lat1,lat2,lat3));
+        }
+
+        public static string Triunghi(int a, int b, int c  )
+
+        {
+            if ((a + b > c) || (a + c > b) || (b + c > a))
+            {
+                string da = "Este triunghi.";
+                return da;
+            }
+
+            else
+            {
+                string nu = "Nu poate fi triunghi.";
+                return nu;
+            }
+        }
+    }
+}
---
+++ ex7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, a1, b1;
+            Console.Write("Prima valoare: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("A doua valoare: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(a + " " + b);
+            a1 = b;
+            b1 = a;
+            Console.WriteLine(a1 + " " + b1);
+
+        }
+    }
+}
