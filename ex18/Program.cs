5ab6d26dbf7c14c0840301a703573da12d02259e
Rad-Paul <115529048+Rad-Paul@users.noreply.github.com>
2023-01-30 05:43:49 +0200

    Add files via upload

---
+++ ex18/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ex18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nr;
+            Console.WriteLine("Introduceti numarul: ");
+            nr = int.Parse(Console.ReadLine());
+            int count2, count3, count5, count7;
+            count2 = count3 = count5 = count7 = 0;
+
+            for(int i = 1; i <= nr; i++)
+            {
+                if(nr % 2 == 0)
+                {
+                    count2++;
+                    nr = nr / 2;
+                }
+
+                else if(nr % 3 == 0)
+                {
+                    count3++;
+                    nr = nr / 3;
+                }
+
+                else if(nr % 5 == 0)
+                {
+                    count5++;
+                    nr = nr / 5;
+                }
+
+                else if (nr % 7 == 0)
+                {
+                    count7++;
+                    nr = nr / 7;
+                }
+
+            }
+
+            Console.WriteLine(count2 + " " + count3 + " " + count5 + " " + count7);
+        }
+    }
+}
---
+++ ex21/ex21/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace ex21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ghiciti un numar intre 1 si 1024 prin intrebari de forma "numarul este mai mare sau egal decat x?". 
+
+            int raspuns = 0;
+            int upperLimit = 1024;
+            int lowerLimit = 1;
+            int nr = 512;
+
+            Console.WriteLine("Programul ghiceste numarul dorit intre 1 si 1024: ");
+            while (raspuns < 3) 
+            {
+                Console.WriteLine("Numarul este mai mare decat " + nr + " \n 1 pentru da\n 2 pentru nu\n 3 pentru raspuns corect");
+                raspuns = int.Parse(Console.ReadLine());
+                if(raspuns == 1)
+                {
+                    lowerLimit = nr;
+                    nr = lowerLimit + (lowerLimit / 2);
+                }
+
+                else if(raspuns == 2)
+                {
+                    upperLimit = nr;
+                    nr = upperLimit / 2;
+                    Console.WriteLine(nr);
+                }
+
+                else if(raspuns == 3)
+                {
+                    Console.WriteLine(nr);
+                }
+            }
+            
+
+        }
+    }
+}
+
+
